ed889e52e832945407fe5689aeb9195a445f6dd9
thomasarsouze <thomas.arsouze@zoho.com>
2024-04-05 13:03:51 +0200

    Change multiprocessing with ray

--- scripts/get_chelsa2.py
+++ scripts/get_chelsa2.py
@@ -21,14 +21,14 @@ aggregation = snakemake.params.aggregation
 # time_frequency = "mon"
 # temp_fold = "results/baselines/chelsa2_tmp"
 # base_years = "2004-2007"
-# nc_files = ["results/baselines/New-Caledonia_chelsa2_monthly-means_1980-2005.nc", 
-#             "results/baselines/New-Caledonia_chelsa2_monthly-means_2006-2019.nc", 
-#             "results/baselines/Vanuatu_chelsa2_monthly-means_1980-2005.nc", 
+# nc_files = ["results/baselines/New-Caledonia_chelsa2_monthly-means_1980-2005.nc",
+#             "results/baselines/New-Caledonia_chelsa2_monthly-means_2006-2019.nc",
+#             "results/baselines/Vanuatu_chelsa2_monthly-means_1980-2005.nc",
 #             "results/baselines/Vanuatu_chelsa2_monthly-means_2006-2019.nc"]
 # cores=3
 # periods= ["1980-2005", "2006-2019"]
 # aggregation="monthly-means"
-        
+
 # libs
 import os
 import shutil
@@ -37,9 +37,10 @@ import rioxarray as rio
 import pandas as pd
 import geopandas
 import datetime
-import multiprocessing as mp
-        
+import ray
+
 # funs
+@ray.remote
 def get_year(year, areas, var, time_freq, month_max, temp_fold):
         areas_names = list(map(lambda x: x.NAME_0.values[0], areas))
         print("Getting year " + str(year) + " for variables " + var + " and areas " + str(areas_names))
@@ -49,47 +50,47 @@ def get_year(year, areas, var, time_freq, month_max, temp_fold):
                 url = 'https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/' + time_freq + '/' + var + "/" \
                         + 'CHELSA_' + var + '_' + '%02d' % (month,) + '_' + str(year) + "_V.2.1.tif"
                 ds = rio.open_rasterio(url, decode_coords="all").to_dataset('band').rename_vars({1 : var})
-                for i, _ in enumerate(areas): 
-                        a[areas_names[i]].append(ds.rio.clip_box(minx=areas_bounds[i].minx[0], 
+                for i, _ in enumerate(areas):
+                        a[areas_names[i]].append(ds.rio.clip_box(minx=areas_bounds[i].minx[0],
                                                                  miny=areas_bounds[i].miny[0],
-                                                                 maxx=areas_bounds[i].maxx[0], 
+                                                                 maxx=areas_bounds[i].maxx[0],
                                                                  maxy=areas_bounds[i].maxy[0]))
         paths = {}
-        for area_name in areas_names: 
+        for area_name in areas_names:
                 ds_year = xr.concat(a[area_name], pd.Index(pd.date_range(datetime.datetime(year, 1, 1), periods=month_max, freq="M"), name="time"))
                 del a[area_name]
                 ds_year = ds_year[["time", "x", "y", var]]
                 if var == "pr":
                         ds_year[var].values = ds_year[var].values * 0.01
-                        ds_year.pr.attrs = {'standard_name': 'precipitation', 
+                        ds_year.pr.attrs = {'standard_name': 'precipitation',
                                            'long_name': 'Monthly precipitation',
-                                           'units': 'mm month-1', 
+                                           'units': 'mm month-1',
                                            'explanation' : 'Precipitation in the earth\'s atmosphere, monthly means precipitation of water in all phases.'}
                 elif var == "tas":
                         ds_year[var] = ds_year[var] * 0.1 - 273.15
-                        ds_year.tas.attrs = {'standard_name': 'temperature at surface', 
+                        ds_year.tas.attrs = {'standard_name': 'temperature at surface',
                                             'long_name': 'Monthly mean daily air temperature',
-                                            'units': '°C', 
+                                            'units': '°C',
                                             'explanation' : 'Monthly mean air temperatures at 2 meters.'}
                 elif var == "tasmin":
                         ds_year[var] = ds_year[var] * 0.1 - 273.15
-                        ds_year.tasmin.attrs = {'standard_name': 'minimum temperature at surface', 
+                        ds_year.tasmin.attrs = {'standard_name': 'minimum temperature at surface',
                                                'long_name': 'Monthly minimum daily air temperature',
-                                               'units': '°C', 
+                                               'units': '°C',
                                                'explanation' : 'Monthly minimum air temperatures at 2 meters.'}
                 elif var == "tasmax":
                         ds_year[var] = ds_year[var] * 0.1 - 273.15
-                        ds_year.tasmax.attrs = {'standard_name': 'maximum temperature at surface', 
+                        ds_year.tasmax.attrs = {'standard_name': 'maximum temperature at surface',
                                                'long_name': 'Monthly maximum daily air temperature',
-                                               'units': '°C', 
+                                               'units': '°C',
                                                'explanation' : 'Monthly maximum air temperatures at 2 meters.'}
-                else:   
+                else:
                         "Problem, variable "+ var + " not recognized, you should use one of the following: tas, tasmin, tasmax, pr."
                 paths[area_name] = temp_fold + "/" + area_name + "_" + var + "_" + str(year) + ".nc"
                 ds_year.chunk({'time':1, 'x':200, 'y':200}).to_netcdf(paths[area_name])
                 del ds_year
         return(paths)
-  
+
 # code
 os.mkdir(temp_fold)
 
@@ -101,7 +102,7 @@ years = list(range(base_years[0], base_years[1]+1))
 if "pr" in variables and base_years[1] >= 2013:
         years.remove(2013) # issue with the tif
 if "pr" in variables and base_years[1] >= 2016:
-        years.remove(2016) # issue with the tif                
+        years.remove(2016) # issue with the tif
 
 if time_frequency == "mon":
         tf = "monthly"
@@ -109,18 +110,17 @@ if time_frequency == "mon":
 if time_frequency != "mon":
         raise Exception("Currently only monthly time frequency available!")
 
-pool = mp.Pool(cores)
-paths=[]
+ray.init(num_cpus=cores, ignore_reinit_error=True)
 for v in variables:
-        paths.append(pool.starmap_async(get_year, [(y, areas, v, tf, months, temp_fold) for y in years]).get())
-pool.close()
+        paths_obj = [get_year.remote(y, areas, v, tf, months, temp_fold) for y in years]
+paths = ray.get(paths_obj)
+ray.shutdown()
 paths2 = {key: list() for key in areas_names}
 for path in paths:
-        for p in path:
-                for area_name in areas_names:
-                        paths2[area_name].append(p[area_name])
+        for area in areas_names:
+                paths2[area].append(path[area])
 del paths
-      
+
 for i, area_name in enumerate(areas_names):
         print("Merging files for area " + area_name + "...")
         ds=xr.open_mfdataset(paths2[area_name], decode_coords="all", parallel=True)
@@ -130,5 +130,5 @@ for i, area_name in enumerate(areas_names):
                 ds_a = ds.sel(time=slice(dmin, dmax)).groupby("time.month").mean("time")
                 path = os.path.dirname(nc_files[0]) + "/" + area_name + "_chelsa2_" + aggregation + "_" + period + ".nc"
                 ds_a.to_netcdf(path)
-        
+
 shutil.rmtree(temp_fold)
--- scripts/get_cordex2.py
+++ scripts/get_cordex2.py
@@ -24,7 +24,7 @@ aggregation = snakemake.params.aggregation
 temp_fold = snakemake.params.tmp
 
 # test
-# base_files = ["results/baselines/New-Caledonia_chelsa2_monthly-means_1980-2005.nc", 
+# base_files = ["results/baselines/New-Caledonia_chelsa2_monthly-means_1980-2005.nc",
 #               "results/baselines/Vanuatu_chelsa2_monthly-means_1980-2005.nc"]
 # check_file = "results/projections/CORDEX_AUS-22_CLMcom-HZG_MOHC-HadGEM2-ES_rcp26_r1i1p1_CCLM5-0-15_v1_chelsa2_done.txt"
 # areas = ["New-Caledonia", "Vanuatu"]
@@ -43,7 +43,7 @@ temp_fold = snakemake.params.tmp
 # periods= ["1980-2005", "2006-2019", "2071-2100"]
 # aggregation="monthly-means"
 # temp_fold="results/projections/CORDEX_AUS-22_CLMcom-HZG_MOHC-HadGEM2-ES_rcp26_r1i1p1_CCLM5-0-15_v1_chelsa2_tmp/"
- 
+
 # libs
 import os
 import re
@@ -56,9 +56,10 @@ import xarray as xr
 import xesmf as xe
 from datetime import datetime as dt
 import numpy
-import multiprocessing as mp
+import ray
 
 # funs
+@ray.remote
 def get_nc(scripts, i):
   script_name = "wget_" + str(i) + ".sh"
   with open(temp_fold + script_name, "w") as writer:
@@ -96,39 +97,40 @@ all_scripts = list(map(lambda res: res.file_context().get_download_script(), ctx
 
 # download
 os.mkdir(temp_fold)
-pool = mp.Pool(cores)
-pool.starmap_async(get_nc, [(all_scripts, i) for i in list(range(len(all_scripts)))]).get()
-pool.close()
+ray.init(num_cpus=cores, ignore_reinit_error=True)
+job_objs = [get_nc.remote(all_scripts, i) for i in list(range(len(all_scripts)))]
+ray.get(job_objs)
+ray.shutdown()
 
 # read & prepare
 files = [temp_fold + f for f in os.listdir(temp_fold) if re.match(r'.*\.(nc)', f)]
 ds = xr.open_mfdataset(files, parallel=True)
 cf = type(ds["time"].values[0]) is not numpy.datetime64
 if cf: # only cftime if not dt but should include more cases
-  ds['time'] = [*map(convert_cf_to_dt, ds.time.values)] 
+  ds['time'] = [*map(convert_cf_to_dt, ds.time.values)]
 if 'pr' in list(ds.keys()):
   ds['pr'] = ds.pr * 60*60*24*ds.time.dt.days_in_month  # s-1 to month-1
-  ds.pr.attrs = {'standard_name': 'precipitation', 
+  ds.pr.attrs = {'standard_name': 'precipitation',
                  'long_name': 'Monthly precipitation',
-                 'units': 'mm month-1', 
+                 'units': 'mm month-1',
                  'explanation' : 'Precipitation in the earth\'s atmosphere, monthly means precipitation of water in all phases.'}
 if 'tas' in list(ds.keys()):
   ds['tas'] = ds.tas - 273.15  # K to °C
-  ds.tas.attrs = {'standard_name': 'temperature at surface', 
+  ds.tas.attrs = {'standard_name': 'temperature at surface',
                   'long_name': 'Monthly mean daily air temperature',
-                  'units': '°C', 
+                  'units': '°C',
                   'explanation' : 'Monthly mean air temperatures at 2 meters.'}
 if 'tasmin' in list(ds.keys()):
   ds['tasmin'] = ds.tasmin - 273.15  # K to °C
-  ds.tasmin.attrs = {'standard_name': 'minimum temperature at surface', 
+  ds.tasmin.attrs = {'standard_name': 'minimum temperature at surface',
                'long_name': 'Monthly minimum daily air temperature',
-               'units': '°C', 
+               'units': '°C',
                'explanation' : 'Monthly minimum air temperatures at 2 meters.'}
 if 'tasmax' in list(ds.keys()):
   ds['tasmax'] = ds.tasmax - 273.15  # K to °C
-  ds.tasmax.attrs = {'standard_name': 'maximum temperature at surface', 
+  ds.tasmax.attrs = {'standard_name': 'maximum temperature at surface',
                      'long_name': 'Monthly maximum daily air temperature',
-                     'units': '°C', 
+                     'units': '°C',
                      'explanation' : 'Monthly maximum air temperatures at 2 meters.'}
 
 # regrid and write per country
